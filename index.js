3b1baec6f0bf276be7e20223823a53964b90ed2d
Hong <qs1225qs@naver.com>
2023-10-20 00:43:17 +0900

    API 불러오기 성공

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const options = {
   };
   
   fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
+    .then((response) => response.json())
+    .then((data) => {
+      const $movieCardList = document.querySelector(".cardList");
+
+      data.results.forEach((movie) => {
+        const $movieCard = document.createElement("div");
+        $movieCard.className = "movie-card";
+        $movieCard.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+          <h3>${movie.title}</h3>
+          <p>${movie.overview}</p>
+          <p>ratin : ${movie.vote_average}</p>`;
+
+      $movieCardList.appendChild($movieCard);
+      });
+    })
+      
+
     .catch(err => console.error(err));
+
